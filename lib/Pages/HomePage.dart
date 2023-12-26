d35141d28de845f35a12cd7bb0aa92286fcb20e9
Kritikaadhikarii <itskritikaaa@gmail.com>
2023-12-26 11:22:25 +0545

    new codes added

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -1,11 +1,28 @@
+
+import 'package:compliment_app/Controllers/ThemeController.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key); // Fix the constructor syntax
 
   @override
   Widget build(BuildContext context) {
+    ThemeController themeController = Get.put(ThemeController());
     return Scaffold(
-      body: Column(children: []),
+      body: SafeArea(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                themeController.changeTheme();
+              },
+              child: Text("Change Theme"), // Add a colon here
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
